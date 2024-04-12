f8b2f92bc1ffe060a289466061cab97e724688ba
喜气洋洋 <13285325+yy855@user.noreply.gitee.com>
2024-04-12 22:16:34 +0800

    完善播放器的功能

--- src/components/MusicPlayer.vue
+++ src/components/MusicPlayer.vue
@@ -23,35 +23,112 @@
           <span class="text-sm font-light">{{ singer }}</span>
         </div>
         <!-- 播放控制 -->
-        <div class="flex flex-col ml-auto my-auto mr-4 w-2/3 h-4/5">
-          <div class="flex h-2/3 justify-around">
-            <!-- <button class="w-10 h-10 bg-slate-500 rounded-full"></button> -->
-            <div class="w-4/5 justify-around">
-              <div class="w-12 h-12 bg-slate-100 rounded-full my-auto" @click="togglePlay">
-              <svg class="w-12 h-12" v-if="!isPlaying" viewBox="0 0 48 48" fill="none"
-                xmlns="http://www.w3.org/2000/svg">
-                <path d="M15 24V11.8756L25.5 17.9378L36 24L25.5 30.0622L15 36.1244V24Z" fill="none" stroke="#333"
-                  stroke-width="2" stroke-linejoin="round" />
-              </svg>
-              <svg class="w-12 h-12" v-else viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M16 12V36" stroke="#333" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                <path d="M32 12V36" stroke="#333" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-              </svg>
-              </div>
+        <div class="flex flex-col ml-auto my-auto mr-4 w-1/2 h-4/5">
+          <div class="flex h-2/3 justify-around w-1/2 mx-auto">
+            <!--播放上一首-->
+            <div class="tooltip my-auto" data-tip="上一首">
+              <button class="btn glass btn-sm">
+                <svg width="28" height="28" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M34 36L22 24L34 12" stroke="#333" stroke-width="4" stroke-linecap="round"
+                    stroke-linejoin="round" />
+                  <path d="M14 12V36" stroke="#333" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+              </button>
             </div>
-            <button class="btn btn-outline btn-primary btn-sm my-auto ml-2" @click="minimizePlayer">下载</button>
-            <button class="btn btn-outline btn-info btn-sm my-auto ml-2" @click="minimizePlayer">缩小</button>
-            <button class="btn btn-outline btn-secondary btn-sm my-auto ml-2" @click="minimizePlayer">全屏</button>
+            <div class="tooltip my-auto" data-tip="播放与暂停">
+              <button class="btn glass" @click="togglePlay">
+                <svg v-if="!isPlaying" width="40" height="40" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M15 24V11.8756L25.5 17.9378L36 24L25.5 30.0622L15 36.1244V24Z" fill="#333" stroke="#333"
+                    stroke-width="4" stroke-linejoin="round" />
+                </svg>
+                <svg v-else width="40" height="40" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M16 12V36" stroke="#333" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+                  <path d="M32 12V36" stroke="#333" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+              </button>
+            </div>
+            <!--播放下一首-->
+            <div class="tooltip my-auto" data-tip="下一首">
+              <button class="btn glass btn-sm">
+                <svg width="28" height="28" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M14 12L26 24L14 36" stroke="#333" stroke-width="4" stroke-linecap="round"
+                    stroke-linejoin="round" />
+                  <path d="M34 12V36" stroke="#333" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+              </button>
+            </div>
           </div>
           <!-- 进度条 -->
           <div class="flex mt-2">
-            <input type="range" class="w-full ml-1 range range-accent range-xs" min="0" :max="durationInSeconds" v-model="currentTimeInSeconds"
-              @input="seek" />
+            <input type="range" class="w-full ml-1 range range-accent range-xs" min="0" :max="durationInSeconds"
+              v-model="currentTimeInSeconds" @input="seek" />
           </div>
           <div class="flex justify-between mt-1">
             <span class="text-xs font-light">{{ currentTime }}</span>
             <span class="text-xs font-light text-right">{{ duration }}</span>
           </div>
+        </div>
+        <!-- 其他控制 -->
+        <div class="w-1/4 flex justify-around">
+          <!--控制播放速度-->
+          <div class="tooltip my-auto" data-tip="播放速度">
+            <button class="btn glass btn-sm">
+              <svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M34.0234 6.68921C31.0764 4.97912 27.6525 4 24 4C12.9543 4 4 12.9543 4 24C4 35.0457 12.9543 44 24 44C35.0457 44 44 35.0457 44 24C44 20.3727 43.0344 16.9709 41.3461 14.0377"
+                  stroke="#333" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+                <path
+                  d="M31.9498 16.0503C31.9498 16.0503 28.5621 25.0948 27.0001 26.6569C25.438 28.219 22.9053 28.219 21.3432 26.6569C19.7811 25.0948 19.7811 22.5621 21.3432 21C22.9053 19.4379 31.9498 16.0503 31.9498 16.0503Z"
+                  fill="none" stroke="#333" stroke-width="4" stroke-linejoin="round" />
+              </svg>
+            </button>
+          </div>
+          <!--控制播放方式-->
+          <div class="tooltip my-auto" data-tip="播放方式">
+            <button class="btn glass btn-sm">
+              <svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M43.8233 25.2305C43.7019 25.9889 43.5195 26.727 43.2814 27.4395C42.763 28.9914 41.9801 30.4222 40.9863 31.6785C38.4222 34.9201 34.454 37 30 37H16C9.39697 37 4 31.6785 4 25C4 18.3502 9.39624 13 16 13H44"
+                  stroke="#333" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M38 7L44 13L38 19" stroke="#333" stroke-width="4" stroke-linecap="round"
+                  stroke-linejoin="round" />
+              </svg>
+            </button>
+          </div>
+          <!--控制音量-->
+          <div class="my-auto flex">
+            <button class="btn glass my-auto btn-sm">
+              <svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M24 3.99976V43.9998" stroke="#333" stroke-width="4" stroke-linecap="round" />
+                <path d="M34 11.9998V35.9998" stroke="#333" stroke-width="4" stroke-linecap="round" />
+                <path d="M4 17.9998V29.9998" stroke="#333" stroke-width="4" stroke-linecap="round" />
+                <path d="M44 17.9998V29.9998" stroke="#333" stroke-width="4" stroke-linecap="round" />
+                <path d="M14 11.9998V35.9998" stroke="#333" stroke-width="4" stroke-linecap="round" />
+              </svg>
+            </button>
+            <input type="range" min="0" max="100" value="40" class="range range-xs my-auto" v-model="volume" @input="changeVolume" />
+          </div>
+          <!--缩小播放器-->
+          <div class="tooltip my-auto" data-tip="最小化">
+            <button class="btn glass btn-sm" @click="minimizePlayer">
+              <svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M27 9V21H39" stroke="#333" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M21 39V27H9" stroke="#333" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M27 21L42 6" stroke="#333" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M21 27L6 42" stroke="#333" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+            </button>
+          </div>
+          <!--全屏播放器-->
+          <div class="tooltip my-auto" data-tip="全屏显示">
+            <button class="btn glass btn-sm">
+              <svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M22 42H6V26" stroke="#333" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M26 6H42V22" stroke="#333" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+            </button>
+          </div>
+
         </div>
       </div>
     </div>
@@ -83,6 +160,13 @@ const currentTimeInSeconds = ref(0);
 const audioPlayer = ref(null);
 
 const isMinimized = ref(false);
+
+const volume = ref(40);
+
+//改变音量
+const changeVolume = () =>{
+  audioPlayer.value.volume = volume.value / 100;
+}
 
 console.log(source.value);
 
@@ -148,20 +232,24 @@ function expandPlayer() {
 .player-transition1-leave-active {
   transition: all 3s ease;
 }
+
 .player-transition1-enter,
 .player-transition1-leave-to {
   opacity: 0;
-  transform: translateY(100%); /* 初始时从下方移动进入或离开 */
+  transform: translateY(100%);
+  /* 初始时从下方移动进入或离开 */
 }
 
 .player-transition1-enter-active,
 .player-transition1-leave-active {
   transition: all 1s ease;
 }
+
 .player-transition1-enter,
 .player-transition1-leave-to {
   opacity: 1;
-  transform: translateY(-10); /* 从下方移动到正常位置 */
+  transform: translateY(-10);
+  /* 从下方移动到正常位置 */
 }
 
 
