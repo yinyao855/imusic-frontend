d75593a05286594b3ad0f740ad78e5b1a2355a1a
薛惠天 <14160885+xue-ht@user.noreply.gitee.com>
2024-04-13 22:55:54 +0800

    push

--- src/components/Music_Play.vue
+++ src/components/Music_Play.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="outcontainer bg-gradient-to-r from-blue-500 to-green-500">
+  <div :class="gradientClass" style="display: grid; height:100%; grid-template-columns: 1fr 1fr;">
     <div class="col1">
       <div class="button_div">
         <buttonchangesize style="display: block" @fullsize="changesize"></buttonchangesize>
@@ -154,52 +154,17 @@ const props=defineProps({
   singer:String,
   name:String,
   lyrics:Array,
+  color1:String,
+  color2:String,
 })
-// const selectedFile = ref(null);
-// const audioSrc = ref('');
+const gradientClass = `bg-gradient-to-r from-blue-500 to-green-500`;
+
 const isPlaying = ref(false);
 const durationtime = ref('0:00');
 const currenttime = ref('0:00');
 const currentduration = ref(0);
 const maxlength = ref(100);
-// const lrcContent = `
-// [00:00.60]关淑怡 - 难得有情人
-// [00:01.60]词：向雪怀
-// [00:02.60]曲：卢东尼
-// [00:29.74]如早春初醒 催促我的心
-// [00:34.95]将不可再等
-// [00:39.00]含情待放那岁月
-// [00:42.06]空出了痴心 令人动心
-// [00:49.57]幸福的光阴 它不会偏心
-// [00:54.77]将分给每颗心
-// [00:58.73]情缘亦远亦近 将交错一生
-// [01:04.39]情侣爱得更甚
-// [01:09.65]甜蜜地与爱人 风里飞奔
-// [01:14.25]高声欢呼你有情 不枉这生
-// [01:19.25]一声你愿意 一声我愿意
-// [01:24.50]惊天爱再没遗憾
-// [01:29.50]明月雾里照人 相爱相亲
-// [01:34.40]让对对的恋人 增添性感
-// [01:39.41]一些恋爱变恨
-// [01:41.56]更多恋爱故事动人
-// [01:44.01]划上了丝丝美感
-// [02:08.84]幸福的光阴 它不会偏心
-// [02:14.04]将分给每颗心
-// [02:18.05]情缘亦远亦近 将交错一生
-// [02:23.55]情侣爱得更甚
-// [02:28.90]甜蜜地与爱人 风里飞奔
-// [02:33.60]高声欢呼你有情 不枉这生
-// [02:38.55]一声你愿意 一声我愿意
-// [02:43.70]惊天爱再没遗憾
-// [02:48.71]明月雾里照人 相爱相亲
-// [02:53.71]让对对的恋人 增添性感
-// [02:58.61]一些恋爱变恨
-// [03:00.71]更多恋爱故事动人
-// [03:03.28]划上了丝丝美感
-// [03:08.43]一些恋爱变恨
-// [03:10.63]更多恋爱故事动人
-// [03:13.13]划上了丝丝美感
-// `;
+
 const lyricsshow = ref([{text: '', special: false}, {text: '', special: false}, {text: '', special: false}, {
   text: '',
   special: false
