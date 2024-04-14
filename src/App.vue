e9b2c76e366bed762955d7aa863b401599a37c18
薛惠天 <14160885+xue-ht@user.noreply.gitee.com>
2024-04-14 22:22:33 +0800

    改个小问题

--- src/App.vue
+++ src/App.vue
@@ -161,9 +161,7 @@ const updateTime = () => {
   const minutes = Math.floor(audio.currentTime / 60);
   const seconds = Math.floor(audio.currentTime % 60);
   currentTime.value = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-  let minute=Math.floor(audio.duration / 60);
-  let second=Math.floor(audio.duration % 60);
-  duration.value = `${minute}:${second < 10 ? '0' : ''}${second}`;
+  duration.value = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
   currentTimeInSeconds.value = audio.currentTime;
   durationInSeconds.value = audio.duration;
 };
