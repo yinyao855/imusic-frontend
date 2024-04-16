632ea9b5a2e9291ce09dc2d2af37622bbd598a7f
薛惠天 <14160885+xue-ht@user.noreply.gitee.com>
2024-04-16 14:34:59 +0800

    menu_button

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,49 @@
 <script setup>
 import MusicPlayer from "./components/MusicPlayer.vue";
+import menu_button from "./components/menu_button.vue";
 import Upload from "./components/Upload.vue";
-import {ref} from "vue";
+import { ref } from "vue";
 import MusicTest from "./components/MusicTest.vue";
-import {musicList} from "./js/lyrics.js";
+import { musicList } from "./js/lyrics.js";
 import lyrics from "./js/lyrics.js";
-import {gradient} from "./js/lyrics.js";
+import { gradient } from "./js/lyrics.js";
 
 const isShow = ref(false);
 
-const curMusicIndex = ref(0)
+const curMusicIndex = ref(0);
 
-const playMusicByChoice = (index) =>{
+const playMusicByChoice = (index) => {
   isShow.value = true;
-  console.log("选择的歌曲"+index);
+  console.log("选择的歌曲" + index);
   curMusicIndex.value = index;
-}
+};
 
 const close = () => {
   isShow.value = false;
 };
+const needshow=ref(false);
+const unfoldmenu = () =>{
+  needshow.value=!needshow.value;
+}
 </script>
 
 <template>
-<!--  <Upload></Upload>-->
-  <MusicTest :musicList="musicList" @playMusic="playMusicByChoice"></MusicTest>
-  <button @click="close" class="fixed top-14 right-14 btn btn-error mx-auto my-auto btn-outline">关闭播放器</button>
+  <menu_button @unfoldmenu="unfoldmenu"></menu_button>
+  <MusicTest :musicList="musicList" @playMusic="playMusicByChoice" v-if="needshow"></MusicTest>
+  <button
+    @click="close"
+    class="fixed top-14 right-14 btn btn-error mx-auto my-auto btn-outline"
+  >
+    关闭播放器
+  </button>
 
-  <MusicPlayer v-if="isShow"
-               v-model:curIndex="curMusicIndex"
-               :musicList="musicList"
-               :gradient="gradient"
-               :lyrics="lyrics"
+  <MusicPlayer
+    v-if="isShow"
+    v-model:curIndex="curMusicIndex"
+    :musicList="musicList"
+    :gradient="gradient"
+    :lyrics="lyrics"
   ></MusicPlayer>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
